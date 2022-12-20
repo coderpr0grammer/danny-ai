3fc0c3ce5df5305f954328e26c4d18f1057d5727
Daniel Martinez <daniellhf@icloud.com>
2022-12-20 06:57:35 -0500

    Initial commit
    don't forget to put your api key in the .env file

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express()
+const port = 3001;
+require('dotenv').config({ path: require('find-config')('.env') })
+const cors = require('cors');
+app.use(cors())
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+	organization: "org-7EI8r48srsW3pVpQ7E7KPmy6",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function request(req) {
+	// console.log(req)
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+	  prompt: req.body,
+	  max_tokens: 256,
+	  temperature: 0,
+  });
+  return { 'result': completion.data.choices[0].text };
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `Suggest three names for an animal that is a superhero.
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: ${capitalizedAnimal}
+Names:`;
+}
+
+
+app.post('/', (req, res) => {
+	console.log(req.body)
+	console.log(process.env.OPENAI_API_KEY)
+	// res.send("hi")
+	// let output = request().then((result) => console.log(result))
+	let output = null;
+	// request(req.body).then((result) => {res.json(result)}).then((data) => {output = data; console.log(data)})
+	
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import logo from './logo.svg';
+
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [GPTResponse, setGPTResponse] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${input}`)
+    // query({prompt: "what is the meaning of life?"}).then((response) => {
+    //   console.log(response) 
+    //   })
+
+    const response = fetch('http://localhost:3001/', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+          'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  body: JSON.stringify({prompt: input})
+}).then((response) => response.json())
+  .then((data) => {console.log(data.result); setGPTResponse(data.result)})
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: "#121212", overflow: "hidden"}}>
+      <form className="inputBox" onSubmit={handleSubmit}>
+        <input type="text" className="input" placeholder="Ask me anything." onChange={(event) => setInput(event.target.value)}></input>
+        <button type="submit" className="submit">Go</button>
+      </form>
+      <br />
+      {GPTResponse ? 
+        <div style={{color: "white"}} className="response">{GPTResponse}</div>
+         : ""}
     </div>
   );
 }
