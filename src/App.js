765299ce31abf6bfb70e93925e0ecc9a5883885e
Daniel Martinez <daniellhf@icloud.com>
2022-12-21 13:14:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
   }
 
   const handleSubmit = (event) => {
+    setInput("")
     setButtonDisabled(1)
     setLoading(1);
     console.log(loading)
@@ -74,7 +75,7 @@ function App() {
     <div className="App">
       <h1 style={{fontFamily: 'monospace', color: "orange", fontSize: "40px"}}>Dani AI</h1>
       <form className="inputBox" onSubmit={handleSubmit}>
-        <textarea type="text" className="input" placeholder="Ask me anything." required onChange={(event) => setInput(event.target.value)}></textarea>
+        <textarea type="text" className="input" placeholder="Ask me anything." required onChange={(event) => setInput(event.target.value)}>{input}</textarea>
         <button type="submit" className="submit" disabled={buttonDisabled}>{loading ? <img src={loader} width="50" height="50"/> : "Go"}</button>
       </form>
       <br />
